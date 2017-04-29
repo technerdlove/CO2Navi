e8c76ad72ec1e9d85108f03609006d04c1a3f7b5
Gary Wong <gawong@microsoft.com>
2017-04-29 05:32:27 -0700

    add options for seeded or random feed

---
+++ feed-seeded.js
@@ -0,0 +1,78 @@
+$.getJSON("feed.json", function (data) {
+
+    for (var i=0; i<data.length; i++) {
+
+        var node = document.createElement("LI");
+
+        var name = data[i].name;
+        var transportation = data[i].transportation;
+        var distanceMiles = data[i].distance;
+        var co2Saved = data[i].co2Saved;
+
+        var transportationPrefix = "";
+        var transportationSuffix = "";
+        if (transportation == "bus") {
+            transportationPrefix = " took the ";
+        } else if (transportation == "car") {
+            transportationPrefix = " drove a ";
+        } else if (transportation == "walk") {
+            transportationPrefix = " ";
+            transportationSuffix = "ed ";
+        } else if (transportation == "bike") {
+            transportationPrefix = " ";
+            transportationSuffix = "d ";
+        }
+        var co2SavedPrefix = " and saved ";
+        var co2SavedSuffix = " lbs of CO2!";
+        var distancePrefix = " ";
+        var distanceSuffix = " miles ";
+        var textnode = document.createTextNode(
+            name +
+            transportationPrefix + transportation + transportationSuffix +
+            distancePrefix + distanceMiles + distanceSuffix +
+            co2SavedPrefix + co2Saved + co2SavedSuffix
+            );
+        node.appendChild(textnode);
+        document.getElementById("feed").appendChild(node);
+
+    }
+
+});
+
+/*
+for(var i=0; i<10; i++) {
+    var node = document.createElement("LI");
+
+    // Randomized data
+    var name = nameArr[Math.floor((Math.random() * nameArr.length))];
+    var transportation = transportationArr[Math.floor((Math.random() * transportationArr.length))];
+    var distanceMiles = Math.floor((Math.random() * 20) + 1);
+    var co2Saved = Math.floor((Math.random() * 20) + 1);
+
+    var transportationPrefix = "";
+    var transportationSuffix = "";
+    if (transportation == "bus") {
+        transportationPrefix = " took the ";
+    } else if (transportation == "car") {
+        transportationPrefix = " drove a ";
+    } else if (transportation == "walk") {
+        transportationPrefix = " ";
+        transportationSuffix = "ed ";
+    } else if (transportation == "bike") {
+        transportationPrefix = " ";
+        transportationSuffix = "d ";
+    }
+    var co2SavedPrefix = " and saved ";
+    var co2SavedSuffix = " lbs of CO2!";
+    var distancePrefix = " ";
+    var distanceSuffix = " miles ";
+    var textnode = document.createTextNode(
+        name +
+        transportationPrefix + transportation + transportationSuffix +
+        distancePrefix + distanceMiles + distanceSuffix +
+        co2SavedPrefix + co2Saved + co2SavedSuffix
+        );
+    node.appendChild(textnode);
+    document.getElementById("feed").appendChild(node);
+}
+*/
